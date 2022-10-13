f186dfcb823ef11661f8c581755c29d0d462086e
bhattaman0001 <bhattaman0001@gmail.com>
2022-10-13 19:40:55 +0530

    initial commit

---
+++ app/src/main/java/com/example/check_in_check_out_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.check_in_check_out_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
