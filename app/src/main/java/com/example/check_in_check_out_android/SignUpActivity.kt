22673077fd132b6598b4905fa37430b6b23a1401
Saransh Baniyal <saranshbaniyal14@gmail.com>
2022-10-14 12:30:14 +0530

    Resolved Issue #1

---
+++ app/src/main/java/com/example/check_in_check_out_android/SignUpActivity.kt
@@ -0,0 +1,24 @@
+package com.example.check_in_check_out_android
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+
+    fun backToHome(view: View?) {
+        startActivity(Intent(this@SignUpActivity, MainActivity::class.java))
+        finish()
+    }
+
+    // end of backToHome method
+    fun goToSignInAct(view: View?) {
+        startActivity(Intent(this@SignUpActivity, SignInActivity::class.java))
+        finish()
+    } // end of goToSignInAct method
+}
