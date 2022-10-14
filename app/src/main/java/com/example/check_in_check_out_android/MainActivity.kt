49beb9887c2d0a047f9d6d872f04fcb825862d0d
samarth2varshney <varshneysamarth2@gmail.com>
2022-10-14 20:12:59 +0530

    Resolved Issue #2

--- app/src/main/java/com/example/check_in_check_out_android/MainActivity.kt
+++ app/src/main/java/com/example/check_in_check_out_android/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.check_in_check_out_android
 
 
-import android.os.Bundle
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var signupButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        signupButton = findViewById(R.id.signup)
+        signupButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/check_in_check_out_android/SignInActivity.kt
+++ app/src/main/java/com/example/check_in_check_out_android/SignInActivity.kt
@@ -3,12 +3,30 @@ package com.example.check_in_check_out_android
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var email:EditText
+    private lateinit var password:EditText
+    private lateinit var sign_in_btn:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+        sign_in_btn = findViewById((R.id.sign_in_btn))
+
+        sign_in_btn.setOnClickListener{
+            val Email = email.text.toString().trim()
+            val Password = password.text.toString().trim()
+
+            if(Email.length<5){
+
+            }
+        }
     }
 
     fun backToHome(view: View?) {
