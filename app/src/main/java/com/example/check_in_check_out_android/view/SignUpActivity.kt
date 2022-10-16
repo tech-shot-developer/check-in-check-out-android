b3b46e34af3dfeb961465cc65f0595f229a85462
Saransh Baniyal <saranshbaniyal14@gmail.com>
2022-10-16 21:31:21 +0530

    Added Logout and Recent Login Button

--- app/src/main/java/com/example/check_in_check_out_android/view/SignUpActivity.kt
+++ app/src/main/java/com/example/check_in_check_out_android/view/SignUpActivity.kt
@@ -3,92 +3,61 @@ package com.example.check_in_check_out_android.view
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.check_in_check_out_android.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
+    private lateinit var user_name: EditText
+    private lateinit var roll_num: EditText
+    private lateinit var phone_num: EditText
+    private lateinit var room_num: EditText
+    private lateinit var email: EditText
+    private lateinit var sign_up_btn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        user_name = binding.userName
+        roll_num = binding.rollNum
+        phone_num = binding.phoneNum
+        room_num = binding.roomNum
+        email = binding.email
+        sign_up_btn = binding.signUpBtn
 
-        binding.signUpBtn.setOnClickListener {
-            processFormField()
+        sign_up_btn.setOnClickListener {
+            val Name = user_name.text.toString().trim()
+            val Roll_number = roll_num.text.toString().trim()
+            val Phone_no = phone_num.text.toString().trim()
+            val Room_no = room_num.text.toString().trim()
+            val Email = email.text.toString().trim()
+
+            if (Name.isEmpty()) {
+                user_name.error = "User Name required"
+                return@setOnClickListener
+            } else if (emailvalid(Email)) {
+                email.error = "Invalid email"
+            } else if (Roll_number.length != 5) {
+
+                roll_num.error = "Enter a roll number"
+                return@setOnClickListener
+            } else if (Room_no.length != 3) {
+                room_num.error = "Enter a valid room number"
+                return@setOnClickListener
+            } else if (Phone_no.length != 10) {
+                phone_num.error = "Enter a valid phone number"
+                return@setOnClickListener
+            } else {
+                Toast.makeText(this, "sign up complete", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
-    private fun processFormField() {
-        if (!validateUserName() || !validateRollNumber() || !validateEmail() || !validateRoomNumber() || !validatePhoneNumber()) {
-            return
-        } // end of check for errors
-
-        // work to do with api
-    }
-
-    private fun validateUserName(): Boolean {
-        val name: String = binding.userName.text.toString()
-        return if (name.isEmpty()) {
-            binding.userName.error = "UserName is Required"
-            false
-        } else {
-            binding.userName.error = null
-            true
-        }
-    }
-
-    private fun validateRollNumber(): Boolean {
-        val rollNumber: String = binding.rollNum.text.toString()
-        return if (rollNumber.length != 5) {
-            binding.rollNum.error = "Please enter a valid roll number"
-            false
-        } else {
-            binding.rollNum.error = null
-            true
-        }
-    }
-
-    private fun validateRoomNumber(): Boolean {
-        val roomNumber: String = binding.roomNum.text.toString()
-        return if (roomNumber.length != 3) {
-            binding.roomNum.error = "Please enter a valid room number"
-            false
-        } else {
-            binding.roomNum.error = null
-            true
-        }
-    }
-
-    private fun validatePhoneNumber(): Boolean {
-        val phoneNumber: String = binding.phoneNum.text.toString()
-        return if (phoneNumber.length != 10) {
-            binding.phoneNum.error = "Please enter a valid phone number"
-            false
-        } else {
-            binding.phoneNum.error = null
-            true
-        }
-    }
-
-    private fun validateEmail(): Boolean {
-        val email_e: String = binding.email.text.toString()
-
-        // check if email is empty
-        return if (email_e.isEmpty()) {
-            binding.email.error = "Email cannot be Empty"
-            false
-        } else if (emailvalid(email_e)) {
-            binding.email.error = "Please enter a valid email"
-            false
-        } else {
-            binding.email.error = null
-            true
-        } // check if email is empty
-    }
-    // end of validateEmail method
-
-    private fun emailvalid(email: String): Boolean {
+    fun emailvalid(email: String): Boolean {
         return !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
