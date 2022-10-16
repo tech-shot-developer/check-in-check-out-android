dc6d98c24dc7746cad0edfdf67e23fb1086ca9e0
bhattaman0001 <bhattaman0001@gmail.com>
2022-10-16 14:37:30 +0530

    new work done

---
+++ app/src/main/java/com/example/check_in_check_out_android/util/constant.kt
@@ -0,0 +1,5 @@
+package com.example.check_in_check_out_android.util
+
+class constant {
+    val geoLocation: String = ""
+}
--- app/src/main/java/com/example/check_in_check_out_android/view/SignInActivity.kt
+++ app/src/main/java/com/example/check_in_check_out_android/view/SignInActivity.kt
@@ -1,11 +1,10 @@
-package com.example.check_in_check_out_android
+package com.example.check_in_check_out_android.view
 
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.example.check_in_check_out_android.R
 
 class SignInActivity : AppCompatActivity() {
 
