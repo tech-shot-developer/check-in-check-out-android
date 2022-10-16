046ae60b495936a9b2ad11835f95ff5514d14c7d
Saransh Baniyal <saranshbaniyal14@gmail.com>
2022-10-16 15:23:56 +0530

    Added Profile Activity

---
+++ app/src/main/java/com/example/check_in_check_out_android/Profile.kt
@@ -0,0 +1,11 @@
+package com.example.check_in_check_out_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
