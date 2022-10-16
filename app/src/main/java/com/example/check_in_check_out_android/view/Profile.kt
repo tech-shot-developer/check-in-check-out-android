467fa97bc410cdc9b30a1178092917391ed6d329
Saransh Baniyal <saranshbaniyal14@gmail.com>
2022-10-16 16:26:13 +0530

    Further Update Profile

--- app/src/main/java/com/example/check_in_check_out_android/view/Profile.kt
+++ app/src/main/java/com/example/check_in_check_out_android/view/Profile.kt
@@ -1,7 +1,8 @@
-package com.example.check_in_check_out_android
+package com.example.check_in_check_out_android.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.check_in_check_out_android.R
 
 class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
