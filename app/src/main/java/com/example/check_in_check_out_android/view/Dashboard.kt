d76781031dc5260811191438b10724525eae63cf
Saransh Baniyal <saranshbaniyal14@gmail.com>
2022-11-08 14:39:55 +0530

    QRcode Generator added to the dashboard

--- app/src/main/java/com/example/check_in_check_out_android/view/Dashboard.kt
+++ app/src/main/java/com/example/check_in_check_out_android/view/Dashboard.kt
@@ -1,11 +1,13 @@
 package com.example.check_in_check_out_android.view
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
-import com.example.check_in_check_out_android.R
+import androidx.appcompat.app.AppCompatActivity
 import com.example.check_in_check_out_android.databinding.ActivityDashboardBinding
-import com.example.check_in_check_out_android.databinding.ActivityMainBinding
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
+
 
 class Dashboard : AppCompatActivity() {
     private lateinit var binding: ActivityDashboardBinding
@@ -17,6 +19,15 @@ class Dashboard : AppCompatActivity() {
         binding.profilebutton.setOnClickListener {
             val intent = Intent(this, Profile::class.java)
             startActivity(intent)
+        }
+        binding.generateBtn.setOnClickListener{
+            val content = "Hello"
+            var bitmap: Bitmap? = null
+            val barcodeEncoder = BarcodeEncoder()
+                bitmap = barcodeEncoder.encodeBitmap(content,
+                    BarcodeFormat.QR_CODE, 300, 300);
+
+            binding.ivQR.setImageBitmap(bitmap)
         }
 
     }
